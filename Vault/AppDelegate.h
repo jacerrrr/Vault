8f3aac7e59ee19d782928e1efcba8c2f3f4aecf2
Jace Allison <jace.allison@gmail.com>
2011-12-22 15:03:13 -0800

    first commit

--- Vault/AppDelegate.h
+++ Vault/AppDelegate.h
@@ -1,10 +1,12 @@
-//
-//  AppDelegate.h
-//  Vault
-//
-//  Created by Jace Allison on 12/22/11.
-//  Copyright (c) 2011 Issaquah High School. All rights reserved.
-//
+/* 
+ * AppDelegate.h
+ * Vault
+ *
+ * Created by Jace Allison on December 21, 2011
+ * Last modified on December 22, 2011 by Jace Allison
+ *
+ * Copyright 2011 Oregon State University. All rights reserved.
+ */
 
 #import <UIKit/UIKit.h>
 
