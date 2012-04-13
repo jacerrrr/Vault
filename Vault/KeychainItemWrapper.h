283f26e2bdb8af5118e57c078875af0680a42ffb
Jace Allison <jace.allison@gmail.com>
2012-04-13 15:37:50 -0700

    Refreshing login works with keychain.

---
+++ Vault/KeychainItemWrapper.h
@@ -0,0 +1,13 @@
+//
+//  KeychainItemWrapper.h
+//  Vault
+//
+//  Created by Jace Allison on 4/7/12.
+//  Copyright (c) 2012 Issaquah High School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeychainItemWrapper : UIWebView
+
+@end
---
+++ Vault/KeychainItemWrapper.m
@@ -0,0 +1,13 @@
+//
+//  KeychainItemWrapper.m
+//  Vault
+//
+//  Created by Jace Allison on 4/7/12.
+//  Copyright (c) 2012 Issaquah High School. All rights reserved.
+//
+
+#import "KeychainItemWrapper.h"
+
+@implementation KeychainItemWrapper
+
+@end
